39b1951a16231ab6ca8e83eca3d7fc4be72efef9
abel-tegegne <abeltegegne8@gmail.com>
2024-09-26 17:49:00 +0300

    implement number comparison

---
+++ compare/compare.py
@@ -0,0 +1,13 @@
+#CONDITIONALS
+"""x=int(input("enter x: "))
+y=int(input("enter y: "))
+if x<y:
+    print("x is less than y")
+
+elif x==y:
+    print("x si equal to y")
+
+else:
+    print("x is greater than y")
+"""
+
