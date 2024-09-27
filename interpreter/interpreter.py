6e87afcd7abb6d81166949f0fddb2c344092de50
abel-tegegne <abeltegegne8@gmail.com>
2024-09-27 10:55:51 +0300

    implementing a math interpreter

---
+++ interpreter/interpreter.py
@@ -0,0 +1,12 @@
+expression=input("Expression: ")
+x, y, z=expression.split(" ")
+x=float(x)
+z=float(z)
+if y=="+":
+    print(round(x+z, 1))
+elif y=="-":
+    print(round(x-z, 1))
+elif y=="*":
+    print(round(x*z, 1))
+elif y=="/" and z!=0:
+    print(round(x/z, 1))
